9e01defff23af9b8bd2e5ee2ad5bb12aa64ef582
jacobgirsky <jgirsky@my.apsu.edu>
2020-04-26 18:56:15 -0500

    Updates

---
+++ precache-manifest.2275bfda920d33e5aae08539c1541930.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "461d82e754759faec7147eec40b2f9d1",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "3a89509558486d66239d",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "247db9aa4e41dee7fbba",
+    "url": "/robofriends/static/css/main.f1948038.chunk.css"
+  },
+  {
+    "revision": "3a89509558486d66239d",
+    "url": "/robofriends/static/js/2.2f7a48c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.2f7a48c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "247db9aa4e41dee7fbba",
+    "url": "/robofriends/static/js/main.d6e31bf5.chunk.js"
+  },
+  {
+    "revision": "8adbf24e3312ffc2ae3e",
+    "url": "/robofriends/static/js/runtime-main.403370f6.js"
+  }
+]);
